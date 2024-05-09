382eeb81000f5d18578fa43fe2b32818d4cd6ebb
Jaime Calvente <calvente.jaime@gmail.com>
2024-05-09 18:56:59 +0200

    API, Core & Infrastructure Projects set up

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{   
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        // Dependency injection => to be able to use the StoreContext
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+    }
+}
